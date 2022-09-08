c81226180185c070a18f3a5061ec76d1645e61cf
fedeh <fedenicohurtado@hotmail.com>
2022-09-07 23:43:43 -0300

    promis

--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import { ItemCount } from '../components/ItemCount'
-import './ItemListContainer.css'
+import React, { useEffect, useState } from 'react';
+
+
+
+import { ItemCount } from '../components/ItemCount';
+import './ItemListContainer.css';
+import { useEffect, useState } from 'react';
 
 export const ItemListContainer = ({ greeting }) => {
+    const [data, setData] = useState ([]);
 
   const handleAdd = () => {
     console.log('se agrego al carrito')
